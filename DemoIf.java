80a59d9d86a620442dcce30e035ac579550d4fda
Lalitha-79 <107744288+Lalitha-79@users.noreply.github.com>
2022-06-25 12:35:17 +0530

    Add files via upload

---
+++ DemoIf.java
@@ -0,0 +1,17 @@
+public class DemoIf
+{
+  public static void main(String args[])
+  {
+    int a=10,b=15,m;
+    if(a>b)
+    { 
+     m=a;
+    }
+    else
+    {
+     m=b;
+    }
+    System.out.println(""max number="+m);
+}
+
+   
