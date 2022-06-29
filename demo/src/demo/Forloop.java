68c3e6d77e2b0cda7eed08845c01087ba5aba424
Mahi508 <mahibavisetti11@gmail.com>
2022-06-29 11:34:14 +0530

    hello

---
+++ demo/src/demo/Forloop.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+	for(int i =200; i<=7999; i++)
+	{
+		System.out.println(i);
+	}
+
+	}
+
+}
---
+++ demo/src/demo/switchchar.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class switchchar {
+
+	public static void main(String[] args) {
+		switch 
+	}
+
+}
